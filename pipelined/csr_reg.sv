8457910f162cfa58e2a8de7b1dba37564fe0c85c
sali-n <138515667+sali-n@users.noreply.github.com>
2024-04-11 11:59:00 +0500

    Update csr_reg.sv

--- pipelined/csr_reg.sv
+++ pipelined/csr_reg.sv
@@ -1,8 +1,16 @@
-module csr_reg(input logic [11:0] addr, input logic [31:0] pc, wdata, input logic clk,reset,wr_en,rd_en,exceptionpins, output logic [31:0] rdata, epc);
+module csr_reg(input logic [11:0] addr, input logic [31:0] pc, wdata, input logic clk,reset,wr_en,rd_en,interupt,is_mret, output logic [31:0] rdata, epc,output logic epctaken);
 	logic [31:0] csr_mip,csr_mie,csr_mstatus, csr_mcause, csr_mtvec, csr_mepc;
 	logic  csr_mip_f,csr_mie_f,csr_mstatus_f, csr_mcause_f, csr_mtvec_f, csr_mepc_f,interupt_csr_en;
 	
-	assign epc = pc;
+
+	always_comb begin //mret 
+		if (is_mret) begin epctaken = 1'b1; epc = csr_mepc; end
+		else if (interupt_csr_en) begin 
+				epctaken = 1'b1;
+				if (csr_mtvec [1:0] == 2'b00) epc = { {csr_mtvec[31:2]}, 2'b00};
+                else if (csr_mtvec [1:0] == 2'b01) epc = { {csr_mtvec[31:2], 2'b00}}  + (csr_mcause << 2); end
+		else begin epctaken=1'b0; epc = pc; end
+	end
 	
 	always_comb begin //read
 		if (rd_en) begin
@@ -18,7 +26,7 @@ module csr_reg(input logic [11:0] addr, input logic [31:0] pc, wdata, input logi
 		end
 	end
 	
-	always_comb begin //write
+	always_comb begin //write flags
 		csr_mip_f = 1'b0;
 		csr_mie_f = 1'b0;
 		csr_mstatus_f = 1'b0;
@@ -34,16 +42,15 @@ module csr_reg(input logic [11:0] addr, input logic [31:0] pc, wdata, input logi
 				12'h342 : csr_mcause_f = 1'b1; //csr_mcause
 				12'h344 : csr_mip_f = 1'b1; // csr mip
 			endcase
-		end
+		end	
 	end
 	
-	always_ff@(negedge reset,posedge clk) begin
-		if (reset) begin csr_mip <= '0; csr_mcause <= '0; csr_mepc <= '0; csr_mtvec <= '0;
-					      csr_mie <= '0; csr_mstatus<= '0; end 
-		else begin 
-		if (csr_mip_f) csr_mip <= wdata;
-		if (csr_mcause_f) csr_mcause <= wdata;
-		if (csr_mepc_f) csr_mepc <= wdata;
+	always_ff@(negedge reset,posedge clk) begin //actually writing to registers!
+		if (reset) begin csr_mip <= '0; csr_mcause <= '0; csr_mepc <= '0; csr_mtvec <= '0; csr_mie <= '0; csr_mstatus<= '0; end 
+		else begin
+		if (interupt) csr_mip[11] = 1'b1; else if (csr_mip_f) csr_mip <= wdata;
+		if (interupt_csr_en) csr_mcause = 32'b1; else if (csr_mcause_f) csr_mcause <= wdata;
+		if (interupt_csr_en) csr_mepc <= pc; else if (csr_mepc_f) csr_mepc <= wdata;
 		if (csr_mtvec_f) csr_mtvec <= wdata;
 		if (csr_mie_f) csr_mie <= wdata;
 		if (csr_mstatus_f) csr_mstatus <= wdata;
